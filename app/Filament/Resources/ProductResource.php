6cc1f59d62183a996dd8ee5bfc95ecf3cad266a7
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-10-16 15:58:09 +0700

    bab 9 episode 4

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProductResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\SpatieTagsInput;
+use Filament\Tables\Columns\TextColumn;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()->schema([
+                    SpatieMediaLibraryFileUpload::make('cover')
+                        ->collection('cover'),
+                    SpatieMediaLibraryFileUpload::make('gallery')
+                        ->collection('cover')
+                        ->multiple(),
+                    TextInput::make('name')
+                        ->label('Product Name'),
+                    TextInput::make('sku')
+                        ->label('SKU')
+                        ->unique(ignoreRecord: true),
+                    TextInput::make('slug')
+                        ->unique(ignoreRecord: true),
+                    SpatieTagsInput::make('tags')
+                        ->type('collection')
+                        ->label('Collection'),
+                    TextInput::make('stock')
+                        ->numeric()->default(0),
+                    TextInput::make('price')
+                        ->numeric()->prefix('Rp'),
+                    TextInput::make('weigth')
+                        ->numeric()->suffix('gram'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('sku'),
+                TextColumn::make('slug'),
+                TextColumn::make('stock'),
+                TextColumn::make('price'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Tags\HasTags;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
-    //
+    use InteractsWithMedia, HasTags;
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('cover')
+            ->fit(Fit::Contain, 300, 300)
+            ->nonQueued();
+    }
 }
