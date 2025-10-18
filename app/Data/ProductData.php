7b6c92c29ec2aa2c2e25799d9fa09af97371aa02
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-10-18 18:35:35 +0700

    DTO Data Transfer Object

---
+++ app/Data/ProductData.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data;
+
+use App\Models\Product;
+use Illuminate\Support\Number;
+use Spatie\LaravelData\Attributes\Computed;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
+
+class ProductData extends Data
+{
+    #[Computed]
+    public string $price_formatted;
+
+    public function __construct(
+        public string $name,
+        public string $short_desc,
+        public string $sku,
+        public string $slug,
+        public string|Optional|null $description,
+        public int $stock,
+        public float $price,
+        public int $weigth,
+        public string $cover_url,
+    ) {
+        $this->price_formatted = Number::currency($price);
+    }
+
+    public static function fromModel(Product $product): self
+    {
+        return new self(
+            $product->name,
+            $product->tags()->where('type', 'collection')->pluck('name')->implode(', '),
+            $product->sku,
+            $product->slug,
+            $product->description,
+            $product->stock,
+            floatval($product->price),
+            $product->weigth,
+            $product->getFirstMediaUrl('cover'),
+        );
+    }
+}
--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Data\ProductData;
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
-        return view('livewire.product-catalog');
+        $result = Product::paginate(9);
+        $products = ProductData::collect($result);
+
+        return view('livewire.product-catalog', compact('products'));
     }
 }
