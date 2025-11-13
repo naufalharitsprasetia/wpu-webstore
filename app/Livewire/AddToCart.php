cd6ec404d8357e026bc12e988161abcc84d1c3a4
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2025-11-13 18:43:18 +0700

    update

--- app/Livewire/AddToCart.php
+++ app/Livewire/AddToCart.php
@@ -2,16 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Contract\CartServiceInterface;
+use App\Data\CartItemData;
+use App\Data\ProductData;
 use Livewire\Component;
 
 class AddToCart extends Component
 {
-    public int $quantity = 1;
+    public int $quantity;
+    public string $sku;
+    public float $price;
+    public int $stock;
+    public int $weigth;
+    public string $label = 'Add to Cart';
 
-
-    public function addToCart()
+    public function mount(ProductData $product, CartServiceInterface $cart)
     {
-        dd($this->quantity);
+        $this->sku = $product->sku;
+        $this->price = $product->price;
+        $this->stock = $product->stock;
+        $this->weigth = $product->weigth;
+        $this->quantity = $cart->getItemBySku($product->sku)->quantity ?? 1;
+    }
+
+    protected function rules(): array
+    {
+        return ['quantity' => ['required', 'integer', 'min:1', "max:{$this->stock}"]];
+    }
+
+    public function addToCart(CartServiceInterface $cart)
+    {
+        $this->validate();
+        $cart->addOrUpdate(new CartItemData(
+            sku: $this->sku,
+            price: $this->price,
+            quantity: $this->quantity,
+            weigth: $this->weigth
+        ));
     }
     public function render()
     {
